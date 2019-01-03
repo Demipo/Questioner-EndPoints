de78c263ca43f1362a3e528c2c305340cad6ef78
Demipo <beronad@gmail.com>
2019-01-03 19:02:06 +0100

    Add the GET to retrieve all meetups

---
+++ meetup.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import db from './db/db';
+import questions_db from './db/questions_db';
+import rsvp_db from './db/rsvp_db';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+//Parse incoming request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+//..................MEETUP SECTION...................
+
+
+//To get all meetups
+app.get('/api/v1/meetups', (request, response) => {
+	response.status(200).send({
+		"status": 200,
+		"meetups": db
+	});
+});

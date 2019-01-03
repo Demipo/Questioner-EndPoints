618e04a460eae81483543c8db2535c105f94aab2
Demipo <beronad@gmail.com>
2019-01-03 18:55:43 +0100

    Add db directory including db files

---
+++ db/questions_db.js
@@ -0,0 +1,13 @@
+
+let questions = [
+                    {
+          			 	"user": 1,
+          			 	"meetup": 1,
+						"title": "This is the new Nigeria.",
+						"body": "Please do remember to come along with your monthly dues.",
+                        "upvote": 5,
+                        "downvote": 2
+			       }
+		   		]
+				
+export default questions; 
---
+++ db/rsvp_db.js
@@ -0,0 +1,10 @@
+
+const rsvp =[
+                {
+          			meetup: 1,
+					topic: "This is the new Nigeria.",
+					status: "Yes"
+			    }
+		    ]
+				
+export default rsvp;

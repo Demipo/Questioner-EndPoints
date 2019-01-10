78399a638ada64b544acd46db9c7ceb79121aa8c
Onaademipo <onaade85@yahoo.com>
2019-01-11 00:15:18 +0100

    Update /models/meetups.js

---
+++ models/meetups.js
@@ -0,0 +1,12 @@
+const value = [
+	{
+		id: 1,
+  		createdOn: '2019-01-02',
+  		topic: 'This is the best time',
+  		location: 'Sheraton Hotels',
+ 	 	happeningOn: '2019-03-03',
+  		tags: ['Bola', 'Seyi', 'Oluwatofunmi'],
+	}
+];
+
+export default value;

761e7647945a1a2a527e37b13adacd253fdd07cb
Onaademipo <onaade85@yahoo.com>
2019-01-11 00:15:47 +0100

    Update /models/questions.js

---
+++ models/questions.js
@@ -0,0 +1,13 @@
+const value = [
+	{
+		id: 1 ,
+		createdOn: '2019-01-09',
+		createdBy: 2,
+		meetup: 3,
+		title: 'Node.js' ,
+		body: 'This is just a question' ,
+		votes : 0 ,
+	}
+];
+
+export default value;
